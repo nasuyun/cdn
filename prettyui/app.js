0298696470f0dd136450ec655c504461d44208c9
HuaB Ma <5532577@qq.com>
2023-05-04 14:06:18 +0800

    searchui update

--- prettyui/app.js
+++ prettyui/app.js
@@ -1,4 +1,4 @@
-const endpoint = "http://pretty.nasuyun.com"
+const endpoint = "https://pretty.nasuyun.com"
 
 const clearIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
 <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
